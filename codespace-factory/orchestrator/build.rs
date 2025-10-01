4e2402c2706ccf8c7aedfb2b9d3caa1418debe10
Kyugito <113055222+Kyugito666@users.noreply.github.com>
2025-10-01 11:35:03 +0700

    Delete codespace-factory directory

--- codespace-factory/orchestrator/build.rs
+++
@@ -1,7 +0,0 @@
-fn main() {
-    cc::Build::new()
-        .cpp(true)
-        .file("../../gh_bridge/src/gh_bridge.cpp")
-        .include("../../gh_bridge/include")
-        .compile("gh_bridge");
-}
